5eb31c01988e77c3906711c28640b78d0510ea6c
tiao <tiao564@hotmail.com>
2016-01-30 00:36:11 -0500

    auto correct for champs added, but its slow, may need to figure out a way to speed it up. fixed some small bugs. Also cleaned up code to look a bit better. Descriptions for functions cleaned up.

--- champLog.py
+++ champLog.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 from __future__ import print_function
+from multiprocessing import Process, Manager, Lock
 import sqlite3
 from decimal import *
 
@@ -12,14 +13,80 @@ conn = sqlite3.connect('Champs.db')
 ################################
 cursor = conn.cursor()
 
+
 def tableCreate():
     cursor.execute("CREATE TABLE matchupData(adTeam TEXT, supTEAM TEXT, adENEMY TEXT, supENEMY TEXT, win INT, loss INT, totalGames INT)")
 
-def dataEntry():
-    #########################################
-    #Get info to intert/ append to the table#
-    #########################################
+champList = ['Aatrox', 'Ahri', 'Akali', 'Alistar', 'Amumu', 'Anivia', 'Annie', 'Ashe', 'Azir', 'Bard', 'Blitzcrank', 'Brand', 'Braum', 'Caitlyn', 'Cassiopeia', 'Cho Gath', 'Corki', 'Darius', 'Diana', 'Djinn', 'Dr Mundo', 'Draven', 'Ekko', 'Elise', 'Evelynn', 'Ezreal', 'Fiddlesticks', 'Fiora', 'Fizz', 'Galio', 'Gangplank', 'Garen', 'Gnar', 'Gragas', 'Hecarim', 'Heimerdinger', 'Illaoi', 'Irelia', 'Janna', 'Jarvan IV', 'Jax', 'Jayce', 'Jinx', 'Kalista', 'Karma', 'Karthus', 'Kassadin', 'Katarina', 'Kayle', 'Kennen', 'Khazix', 'Kindred', 'Kogmaw', 'LeBlanc', 'Lee Sin', 'Leona', 'Lissandra', 'Lucian', 'Lulu', 'Lux', 'Malphite', 'Malzahar', 'Maokai', 'Master Yi', 'Miss Fortune', 'Mordekaiser', 'Morgana', 'Nami', 'Nasus', 'Nautilus', 'Nidalee', 'Nocturne', 'Nunu', 'Olaf', 'Orianna', 'Pantheon', 'Poppy', 'Quinn', 'Rammus', 'Reksai', 'Renekton', 'Rengar', 'Riven', 'Rumble', 'Ryze', 'Sejuani', 'Shaco', 'Shen', 'Shyvana', 'Singed', 'Sion', 'Sivir', 'Skarner', 'Sona', 'Soraka', 'Swain', 'Syndra', 'Tahm Kench', 'Talon', 'Taric', 'Teemo', 'Thresh', 'Tristana', 'Trundle', 'Tryndamere', 'Twisted Fate', 'Twitch', 'Udyr', 'Urgot', 'Varus', 'Vayne', 'Veigar', 'Vi', 'Viktor', 'Vladimir', 'Velkoz', 'Volibear', 'Warwick', 'Wukong', 'Xerath', 'Xin Zhao', 'Yasuo', 'Yorick', 'Zac', 'Zed', 'Ziggs', 'Zilean', 'Zyra']
 
+ ###########################
+#Standardize(string)        # 
+#  takes in a string,       # 
+#  capitalizes each word    #
+#  and removed any uneeded  #
+#  spaces                   #
+#  it also checks against   #
+#  champList and if chanp   #
+#  is not found, it gets    #
+#  best fit                 #
+ ###########################
+def standardize(string):
+    string = string.split(" ")
+    for val in string:
+        if (val == ""):
+            string.remove(val)
+    string = " ".join(string)
+    string = string.title()
+    if string in champList:
+        return string
+    else:
+        lock = Lock()
+        manager = Manager()
+        count = manager.dict()
+        thread_list = []
+        max_score = 0
+        for x in range(len(champList)):
+            p = Process(target=bestFit, args=(string, champList[x], lock, count,))
+            p.start()
+            thread_list.append(p)
+            p.join()
+
+        prox_count = dict(count)
+        for val in prox_count:
+            if(int(prox_count[val] > max_score)):
+                key = val
+                max_score = int(count[val])
+        return key
+
+def not_champ_standardize(string):
+    string = string.split(" ")
+    for val in string:
+        if (val == ""):
+            string.remove(val)
+    string = " ".join(string)
+    string = string.title()
+    return string
+
+def bestFit(string, comp, padl, compare_list):
+    temp_compair = comp
+    score = 0
+    for letter in string:
+        x=0
+        found = False
+        while(x<len(temp_compair) and found==False):
+            if(letter == temp_compair[x]):
+                found = True
+                temp_compair.replace(letter,"")
+                score += 1
+            x += 1
+    padl.acquire()
+    compare_list[comp] = score
+    padl.release()
+
+ #######################################
+#Get info to intert/ append to the table#
+ #######################################
+def dataEntry():
     ###############
     #Ask for input#
     ###############
@@ -38,11 +105,11 @@ def dataEntry():
     # no double entries     #
     # accidentally occur    #
     #########################
-    adTEAM = adTEAM.lower()
-    supTEAM = supTEAM.lower()
-    adENEMY = adENEMY.lower()
-    supENEMY = supENEMY.lower()
-    win_loss = win_loss.lower()
+    adTEAM   = standardize(adTEAM)
+    supTEAM  = standardize(supTEAM)
+    adENEMY  = standardize(adENEMY)
+    supENEMY = standardize(supENEMY)
+    win_loss = not_champ_standardize(win_loss)
 
     ##########################################################
     #We attempt to execute an update to win and total games  #
@@ -56,7 +123,7 @@ def dataEntry():
     #######
     # Win #
     #######
-    if (win_loss=="win"):
+    if (win_loss=="Win"):
         cursor.execute("UPDATE matchupData SET win=win+1 WHERE adTEAM= :adT and supTEAM= :supT and adENEMY= :adE and supENEMY= :supE", {'adT':adTEAM, 'supT':supTEAM, 'adE':adENEMY, 'supE':supENEMY})
         cursor.execute("UPDATE matchupData SET totalGames=totalGames+1 WHERE adTEAM= :adT and supTEAM= :supT and adENEMY= :adE and supENEMY= :supE", {'adT':adTEAM, 'supT':supTEAM, 'adE':adENEMY, 'supE':supENEMY})
         if(cursor.rowcount == 0):
@@ -65,7 +132,7 @@ def dataEntry():
     ########
     # loss #
     ########
-    if (win_loss=="loss"):
+    if (win_loss=="Loss"):
         cursor.execute("UPDATE matchupData SET loss=loss+1 WHERE adTEAM= :adT and supTEAM= :supT and adENEMY= :adE and supENEMY= :supE", {'adT':adTEAM, 'supT':supTEAM, 'adE':adENEMY, 'supE':supENEMY})
         cursor.execute("UPDATE matchupData SET totalGames=totalGames+1 WHERE adTEAM= :adT and supTEAM= :supT and adENEMY= :adE and supENEMY= :supE", {'adT':adTEAM, 'supT':supTEAM, 'adE':adENEMY, 'supE':supENEMY})
         if(cursor.rowcount == 0):
@@ -78,19 +145,20 @@ def dataEntry():
     print ("\nChanges complete\n")
 
 
+
+ ####################################################################################################
+#SELECT (WIN/LOSS/GAMES) from matchupData WHERE adcTEAM=W and supTEAM=X and adcENEMY=Y and supENEMY=Z#
+ ####################################################################################################
 def fetchData():
-    ######################################################################################################
-    #SELECT (WIN/LOSS/GAMES) from matchupData WHERE adcTEAM=W and supTEAM=X and adcENEMY=Y and supENEMY=Z#
-    ######################################################################################################
     print (" 1: ADC VS ADC\n 2: ADC VS SUP\n 3: ADC VS ADC SUP\n 4: SUP VS ADC\n 5: SUP VS SUP\n 6: SUP VS ADC SUP\n 7: ADC SUP VS ADC\n 8: ADC SUP VS SUP\n 9: ADC SUP VS ADC SUP\n all: Dump the whole table!")
     roll_select = raw_input("Pick what match up to get data on: ")
-    roll_select = roll_select.lower()
+    roll_select = not_champ_standardize(roll_select)
     allWins = 0
     allGames = 0
     #######
     # all #
     #######
-    if(roll_select == "all"):
+    if(roll_select == "All"):
         print ("{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}".format("AdTEAM","SupTeam", "AdENEMY", "SupENEMY", "Win", "Loss", "Total Games"))
         for row in cursor.execute("SELECT * FROM matchupData"):
             for i in range(len(row)):
@@ -100,10 +168,10 @@ def fetchData():
     # 1 ADC vs ADC #
     ################
     elif(roll_select == "1"):
-        adTEAM = raw_input("ADC TEAM\n>>")
-        adENEMY = raw_input("ADC ENEMY\n>>")
-        adTEAM = adTEAM.lower()
-        adENEMY = adENEMY.lower()
+        adTEAM   = raw_input("ADC TEAM\n>>")
+        adENEMY  = raw_input("ADC ENEMY\n>>")
+        adTEAM   = standardize(adTEAM)
+        adENEMY  = standardize(adENEMY)
         print ("{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}".format("AdTEAM","SupTeam", "AdENEMY", "SupENEMY", "Win", "Loss", "Total Games"))
         for row in cursor.execute("SELECT * FROM matchupData WHERE adTEAM= :adT and adENEMY= :adE", {'adT':adTEAM, 'adE':adENEMY}):
             for i in range(len(row)):
@@ -119,10 +187,10 @@ def fetchData():
     # 2 ADC vs SUP #
     ################
     elif(roll_select == "2"):
-        adTEAM= raw_input("ADC TEAM\n>>")
+        adTEAM   = raw_input("ADC TEAM\n>>")
         supENEMY = raw_input("SUP ENEMY\n>>")
-        adTEAM = adTEAM.lower()
-        supENEMY = supENEMY.lower()
+        adTEAM   = standardize(adTEAM)
+        supENEMY = standardize(supENEMY)
         print ("{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}".format("AdTEAM","SupTeam", "AdENEMY", "SupENEMY", "Win", "Loss", "Total Games"))
         for row in cursor.execute("SELECT * FROM matchupData WHERE adTEAM= :adT and supENEMY= :supE", {'adT':adTEAM, 'supE':supENEMY}):
             for i in range(len(row)):
@@ -139,19 +207,19 @@ def fetchData():
     ######################
     elif(roll_select == "3"):
         print ('\n\n fuck \n\n')
-        adTEAM = raw_input("ADC TEAM\n>>")
-        adENEMY = raw_input("ADC ENEMY\n>>")
+        adTEAM   = raw_input("ADC TEAM\n>>")
+        adENEMY  = raw_input("ADC ENEMY\n>>")
         supENEMY = raw_input("SUP ENEMY\n>>")
-        adTEAM = adTEAM.lower()
-        adENEMY = adENEMY.lower()
-        supENEMY = supENEMY.lower()
+        adTEAM   = standardize(adTEAM)
+        adENEMY  = standardize(adENEMY)()
+        supENEMY = standardize(supENEMY)
         print ("{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}".format("AdTEAM","SupTeam", "AdENEMY", "SupENEMY", "Win", "Loss", "Total Games"))
         for row in cursor.execute("SELECT * FROM matchupData WHERE adTEAM= :adT and adENEMY= :adE and supENEMY= :supE", {'adT':adTEAM, 'adE':adENEMY, 'supE':supENEMY}):
             for i in range(len(row)):
                 print ("{:>14}".format(str(row[i]).replace('u\'','').replace('(','').replace(')','').replace('\'','')), end="")
-            percent = (Decimal(row[4])/Decimal(row[6])*100)
+            percent  = (Decimal(row[4])/Decimal(row[6])*100)
             print ("\n{:>80}".format("win percentage: %.2f"%percent))
-            allWins = allWins + row[4]
+            allWins  = allWins + row[4]
             allGames = allGames + row[6]
         totalPercent = Decimal(allWins)/Decimal(allGames)*100
         print("\nTotal win loss in the match up: %.2f"%totalPercent)
@@ -162,15 +230,15 @@ def fetchData():
     if(roll_select == "4"):
         supTEAM = raw_input("SUP TEAM\n>>")
         adENEMY = raw_input("ADC ENEMY\n>>")
-        supTEAM = supTEAM.lower()
-        adENEMY = adENEMY.lower()
+        supTEAM = standardize(supTEAM)
+        adENEMY = standardize(adENEMY)()
         print ("{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}".format("AdTEAM","SupTeam", "AdENEMY", "SupENEMY", "Win", "Loss", "Total Games"))
         for row in cursor.execute("SELECT * FROM matchupData WHERE supTEAM= :supT and adENEMY= :adE", {'supT':supTEAM, 'adE':adENEMY}):
             for i in range(len(row)):
                 print ("{:>14}".format(str(row[i]).replace('u\'','').replace('(','').replace(')','').replace('\'','')), end="")
-            percent = (Decimal(row[4])/Decimal(row[6])*100)
+            percent  = (Decimal(row[4])/Decimal(row[6])*100)
             print ("\n{:>80}".format("win percentage: %.2f"%percent))
-            allWins = allWins + row[4]
+            allWins  = allWins + row[4]
             allGames = allGames + row[6]
         totalPercent = Decimal(allWins)/Decimal(allGames)*100
         print("\nTotal win loss in the match up: %.2f"%totalPercent)
@@ -179,17 +247,17 @@ def fetchData():
     # 5 SUP VS SUP #
     ################
     if(roll_select == "5"):
-        supTEAM = raw_input("SUP TEAM\n>>")
+        supTEAM  = raw_input("SUP TEAM\n>>")
         supENEMY = raw_input("SUP ENEMY\n>>")
-        supTEAM = supTEAM.lower()
-        supENEMY = supENEMY.lower()
+        supTEAM  = standardize(supTEAM)
+        supENEMY = standardize(supENEMY)
         print ("{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}".format("AdTEAM","SupTeam", "AdENEMY", "SupENEMY", "Win", "Loss", "Total Games"))
         for row in cursor.execute("SELECT * FROM matchupData WHERE supTEAM= :supT and supENEMY= :supE", {'supT':supTEAM, 'supE':supENEMY}):
             for i in range(len(row)):
                 print ("{:>14}".format(str(row[i]).replace('u\'','').replace('(','').replace(')','').replace('\'','')), end="")
-            percent = (Decimal(row[4])/Decimal(row[6])*100)
+            percent  = (Decimal(row[4])/Decimal(row[6])*100)
             print ("\n{:>80}".format("win percentage: %.2f"%percent))
-            allWins = allWins + row[4]
+            allWins  = allWins + row[4]
             allGames = allGames + row[6]
         totalPercent = Decimal(allWins)/Decimal(allGames)*100
         print("\nTotal win loss in the match up: %.2f"%totalPercent)
@@ -198,19 +266,19 @@ def fetchData():
     # 6 SUP VS ADC + SUP #
     ######################
     if(roll_select == "6"):
-        supTEAM = raw_input("SUP TEAM\n>>")
-        adENEMY = raw_input("ADC ENEMY\n>>")
+        supTEAM  = raw_input("SUP TEAM\n>>")
+        adENEMY  = raw_input("ADC ENEMY\n>>")
         supENEMY = raw_input("SUP ENEMY\n>>")
-        supTEAM = supTEAM.lower()
-        adENEMY = adENEMY.lower()
-        supENEMY = supENEMY.lower()
+        supTEAM  = standardize(supTEAM)
+        adENEMY  = standardize(adENEMY)()
+        supENEMY = standardize(supENEMY)
         print ("{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}".format("AdTEAM","SupTeam", "AdENEMY", "SupENEMY", "Win", "Loss", "Total Games"))
         for row in cursor.execute("SELECT * FROM matchupData WHERE supTEAM= :supT and adENEMY= :adE and supENEMY= :supE", {'supT':supTEAM, 'adE':adENEMY, 'supE':supENEMY }):
             for i in range(len(row)):
                 print ("{:>14}".format(str(row[i]).replace('u\'','').replace('(','').replace(')','').replace('\'','')), end="")
-            percent = (Decimal(row[4])/Decimal(row[6])*100)
+            percent  = (Decimal(row[4])/Decimal(row[6])*100)
             print ("\n{:>80}".format("win percentage: %.2f"%percent))
-            allWins = allWins + row[4]
+            allWins  = allWins + row[4]
             allGames = allGames + row[6]
         totalPercent = Decimal(allWins)/Decimal(allGames)*100
         print("\nTotal win loss in the match up: %.2f"%totalPercent)    
@@ -219,19 +287,19 @@ def fetchData():
     # 7 ADC + SUP VS ADC #
     ######################
     if(roll_select == "7"):
-        adTEAM = raw_input("ADC TEAM\n>>")
+        adTEAM  = raw_input("ADC TEAM\n>>")
         supTEAM = raw_input("SUP TEAM\n>>")
         adENEMY = raw_input("ADC ENEMY\n>>")
-        adTEAM = adTEAM.lower()
-        supTEAM = supTEAM.lower()
-        adENEMY = adENEMY.lower()
+        adTEAM  = standardize(adTEAM)
+        supTEAM = standardize(supTEAM)
+        adENEMY = standardize(adENEMY)()
         print ("{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}".format("AdTEAM","SupTeam", "AdENEMY", "SupENEMY", "Win", "Loss", "Total Games"))
         for row in cursor.execute("SELECT * FROM matchupData WHERE adTEAM= :adT and supTEAM= :supT and adENEMY= :adE", {'adT':adTEAM, 'supT':supTEAM, 'adE':adENEMY }):
             for i in range(len(row)):
                 print ("{:>14}".format(str(row[i]).replace('u\'','').replace('(','').replace(')','').replace('\'','')), end="")
-            percent = (Decimal(row[4])/Decimal(row[6])*100)
+            percent  = (Decimal(row[4])/Decimal(row[6])*100)
             print ("\n{:>80}".format("win percentage: %.2f"%percent))
-            allWins = allWins + row[4]
+            allWins  = allWins + row[4]
             allGames = allGames + row[6]
         totalPercent = Decimal(allWins)/Decimal(allGames)*100
         print("\nTotal win loss in the match up: %.2f"%totalPercent)    
@@ -240,19 +308,19 @@ def fetchData():
     # 8 ADC + SUP VS SUP #
     ######################
     if(roll_select == "8"):
-        adTEAM = raw_input("ADC TEAM\n>>")
-        supTEAM = raw_input("SUP TEAM\n>>")
+        adTEAM   = raw_input("ADC TEAM\n>>")
+        supTEAM  = raw_input("SUP TEAM\n>>")
         supENEMY = raw_input("SUP ENEMY\n>>")
-        adTEAM = adTEAM.lower()
-        supTEAM = supTEAM.lower()
-        supENEMY = supENEMY.lower()
+        adTEAM   = standardize(adTEAM)
+        supTEAM  = standardize(supTEAM)
+        supENEMY = standardize(supENEMY)
         print ("{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}".format("AdTEAM","SupTeam", "AdENEMY", "SupENEMY", "Win", "Loss", "Total Games"))
         for row in cursor.execute("SELECT * FROM matchupData WHERE adTEAM= :adT and supTEAM= :supT and supENEMY= :supE", {'adT':adTEAM, 'supT':supTEAM, 'supE':supENEMY}):
             for i in range(len(row)):
                 print ("{:>14}".format(str(row[i]).replace('u\'','').replace('(','').replace(')','').replace('\'','')), end="")
-            percent = (Decimal(row[4])/Decimal(row[6])*100)
+            percent  = (Decimal(row[4])/Decimal(row[6])*100)
             print ("\n{:>80}".format("win percentage: %.2f"%percent))
-            allWins = allWins + row[4]
+            allWins  = allWins + row[4]
             allGames = allGames + row[6]
         totalPercent = Decimal(allWins)/Decimal(allGames)*100
         print("\nTotal win loss in the match up: %.2f"%totalPercent)  
@@ -261,21 +329,21 @@ def fetchData():
     # 9 ADC + SUP VS ADC + SUP #
     ############################
     if(roll_select == "9"):
-        adTEAM = raw_input("ADC TEAM\n>>")
-        supTEAM = raw_input("SUP TEAM\n>>")
-        adENEMY = raw_input("ADC ENEMY\n>>")
+        adTEAM   = raw_input("ADC TEAM\n>>")
+        supTEAM  = raw_input("SUP TEAM\n>>")
+        adENEMY  = raw_input("ADC ENEMY\n>>")
         supENEMY = raw_input("SUP ENEMY\n>>")
-        adTEAM = adTEAM.lower()
-        adENEMY = adENEMY.lower()
-        supTEAM = supTEAM.lower()
-        supENEMY = supENEMY.lower()
+        adTEAM   = standardize(adTEAM)
+        adENEMY  = standardize(adENEMY)()
+        supTEAM  = standardize(supTEAM)
+        supENEMY = standardize(supENEMY)
         print ("{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}{:>14}".format("AdTEAM","SupTeam", "AdENEMY", "SupENEMY", "Win", "Loss", "Total Games"))
         for row in cursor.execute("SELECT * FROM matchupData WHERE adTEAM= :adT and supTEAM= :supT and adENEMY= :adE and supENEMY= :supE", {'adT':adTEAM, 'supT':supTEAM, 'adE':adENEMY, 'supE':supENEMY}):
             for i in range(len(row)):
                 print ("{:>14}".format(str(row[i]).replace('u\'','').replace('(','').replace(')','').replace('\'','')), end="")
-            percent = (Decimal(row[4])/Decimal(row[6])*100)
+            percent  = (Decimal(row[4])/Decimal(row[6])*100)
             print ("\n{:>80}".format("win percentage: %.2f"%percent))
-            allWins = allWins + row[4]
+            allWins  = allWins + row[4]
             allGames = allGames + row[6]
         totalPercent = Decimal(allWins)/Decimal(allGames)*100
         print("\nTotal win loss in the match up: %.2f"%totalPercent)
@@ -292,7 +360,7 @@ def main():
     try:
         tableCreate()
     except sqlite3.OperationalError:
-        print("")
+        pass
     while(select != "exit"):
         select = raw_input("\ninput, fetch or exit\n>>")    
         select = select.lower()
